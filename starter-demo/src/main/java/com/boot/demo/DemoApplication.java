6163130c616c29aa616dd1fbd050c4ce2f873ebf
1806821598@qq.com <@#6231130>
2019-07-02 22:57:33 +0800

    test

--- starter-demo/src/main/java/com/boot/demo/DemoApplication.java
+++ starter-demo/src/main/java/com/boot/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.boot.demo;
 
-
-import com.boot.demo.annotation.EnableLogFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableLogFilter
-
-public class BootDemoApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootDemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
-   @GetMapping("/test")
-    public String test(){
 
-
-        return "this is a demo boot";
-    }
 
 }
---
+++ starter-demo/src/test/java/com/boot/demo/BootDemoApplicationTests.java
@@ -0,0 +1,10 @@
+package com.boot.demo;
+
+
+
+
+public class BootDemoApplicationTests {
+
+
+
+}
