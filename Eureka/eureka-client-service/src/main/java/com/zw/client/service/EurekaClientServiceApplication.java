1dd6ed29c32db10701d981e5200d9941c40f7184
1806821598@qq.com <samson909785>
2019-07-03 18:47:40 +0800

    添加自定义的starter

---
+++ Eureka/eureka-client-service/src/main/java/com/zw/client/service/EurekaClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zw.client.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientServiceApplication.class, args);
+    }
+
+}
