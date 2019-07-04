a031878cd44d56a54402a1ca8a4cbede5c81387e
1806821598@qq.com <samson909785>
2019-07-04 11:54:16 +0800

    添加自定义的starter

--- Eureka/eureka-server/src/main/java/com/zw/server/EurekaServerApplication.java
+++ Eureka/eureka-server/src/main/java/com/zw/server/EurekaServerApplication.java
@@ -2,10 +2,11 @@ package com.zw.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 //为项目自动配置所必须的类，标示服务为注册中心
-//@EnableEurekaServer
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
