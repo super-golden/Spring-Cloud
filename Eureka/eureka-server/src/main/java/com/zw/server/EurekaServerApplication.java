2dc823e81da654f4e9582850052299e43c222d70
1806821598@qq.com <@#6231130>
2019-07-04 11:03:02 +0800

    修改pom文件

--- Eureka/eureka-server/src/main/java/com/zw/server/EurekaServerApplication.java
+++ Eureka/eureka-server/src/main/java/com/zw/server/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //为项目自动配置所必须的类，标示服务为注册中心
-@EnableEurekaServer
+//@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
