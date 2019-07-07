0c4d1dc1af4cc581e255f36bfe5ef56aa6dcd356
1806821598@qq.com <@#6231130>
2019-07-07 14:29:31 +0800

    修改pom文件

---
+++ Eureka/eureka-client-service/src/main/java/com/zw/client/service/controller/FeignServiceController.java
@@ -0,0 +1,31 @@
+package com.zw.client.service.controller;
+
+
+
+import com.zw.dto.Instance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/feign-service")
+public class FeignServiceController {
+
+
+    private static  final Logger logger = LoggerFactory.getLogger(FeignServiceController.class);
+
+    private static String DEFAULT_SERVICE_ID = "application";
+    private static String DEFAULT_HOST = "localhost";
+    private static int DEFAULT_PORT = 8080;
+
+    @RequestMapping(value = "/instance/{serviceId}" ,method = RequestMethod.GET)
+    public Instance getInstanceByserviceId(@PathVariable String serviceId){
+
+        logger.info("Get Instance by serviceId {}", serviceId);
+        return new Instance(serviceId, DEFAULT_HOST, DEFAULT_PORT);
+    }
+}
