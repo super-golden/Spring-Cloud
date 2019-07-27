ea3476e6a98fee067f3bd77359d93d414484eacc
1806821598@qq.com <samson909785>
2019-07-27 17:29:27 +0800

    日常提交Ribbon

--- Ribbon/ribbon-client/src/main/java/com/zw/client/controller/RibbonController.java
+++ Ribbon/ribbon-client/src/main/java/com/zw/client/controller/RibbonController.java
@@ -4,7 +4,6 @@ import com.zw.client.api.FeignService;
 import com.zw.client.config.RibbonConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
